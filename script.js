fd0fed9e9d16f9d92620b7740cb15268c31b3b78
Eduardo Oliveira <127622263+edujoliver@users.noreply.github.com>
2025-02-19 16:57:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var random = document.querySelector(".random");
+
+function randomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+random.addEventListener("click", function() {
+    color1.value = randomColor();
+    color2.value = randomColor();
+    setGradient();
+});
+
+function setGradient() {
+    body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+    css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient); 
+
+color2.addEventListener("input", setGradient); 
+
